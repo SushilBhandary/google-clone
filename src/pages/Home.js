f8d3032f0442783c01869b029fa7ec9802a99534
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-03-20 00:40:31 +0530

    updating home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,19 @@ import './Home.css'
 import { Link } from 'react-router-dom'
 import AppsIcon from '@mui/icons-material/Apps';
 import Avatar from '@mui/material/Avatar';
-import Search from './Search';
+import Search from '../components/Search';
 
 function Home() {
   return (
     <div className='home'>
         <div className='home__header'>
           <div className='home__headerLeft'>
-            <Link to='/about' >About</Link>
+            <Link to='https://about.google/?utm_source=google-IN&utm_medium=referral&utm_campaign=hp-footer&fg=1' >About</Link>
             <Link to='/store' >Store</Link>
           </div>
           <div className='home__headerRight'>
-            <Link to='/gmail' >Gmail</Link>
-            <Link to='/images' >Images</Link>
+            <Link to='https://mail.google.com/' >Gmail</Link>
+            <Link to='https://www.google.co.in/imghp?hl=en&authuser=0&ogbl' >Images</Link>
             <AppsIcon />
             <Avatar />
           </div>

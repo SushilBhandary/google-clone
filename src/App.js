cabe8345a66e939452756508f118a63ed63d5ebd
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-03-19 23:50:56 +0530

    adding home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
     <div className="app">
-      <h1>HI all</h1>
+      <Router>
+        <Routes>
+        <Route path="/"  element={<Home />} />
+        <Route path="/search"  element={<SearchPage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
+
+
 
 export default App;
